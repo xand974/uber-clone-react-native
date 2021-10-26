7a7c69baa5fd40b73d2244eb165bd231a7043438
Alexandre <xand974@gmail.com>
2021-10-26 19:04:41 +0200

    zoom

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
@@ -7,9 +7,18 @@ import { REACT_APP_GOOGLE_API } from "@env";
 
 export default function Map() {
   const { origin, destination } = useSelector((state) => state.location);
+  const mapRef = useRef(null);
 
+
+  useEffect(() => {
+    if(!origin && !destination)
+    return 
+    mapRef?.current.fitToSuppliedMarkers(["origin", "destination"], {edgePadding : { top : 10 , bottom : 10 , left : 10 , right : 10}})
+
+  }, [origin, destination])
   return (
     <MapView
+      ref={mapRef}
       style={styles.map}
       mapType="mutedStandard"
       initialRegion={{
